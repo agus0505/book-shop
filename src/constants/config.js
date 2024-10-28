98fe91ae7eb2d253119c309be459bdc5555ae2d7
agus0505 <agustin.pecelis@gmail.com>
2024-10-28 10:06:08 -0300

    change config.js

--- src/constants/config.js
+++ src/constants/config.js
@@ -3,10 +3,10 @@ config()
 
 export const PORT = 1234
 export const HOST = `http://localhost:` + PORT
-export const NGROK_HOST = `https://book-shop-ljjd.vercel.app/`
+export const NGROK_HOST = `https://book-shop-mern.onrender.com/`
 export const PAYPAL_API_SECRET = process.env.PAYPAL_API_SECRET
 export const PAYPAL_API_CLIENT = process.env.PAYPAL_API_CLIENT
 export const MERCADO_PAGO_ACCES_TOKEN = process.env.MERCADO_PAGO_ACCES_TOKEN
-export const PAYPAL_API = 'https://api-m.sandbox.paypal.com'
+export const PAYPAL_API = 'https://api-m.paypal.com'
 export const MONGODB_URL = process.env.MONGODB_URL
 
